a91fe4744ef04fae363f502200506b48a8e3e76d
wtricks <wtricks.ak@gmail.com>
2024-10-03 17:29:44 +0530

    added extension info.

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'LinkedIn AI Reply',
+    description: 'Enhance your LinkedIn messaging experience with AI-powered replies.',
+    version: '0.0.1',
+    type: 'popup',
+  }
 });
